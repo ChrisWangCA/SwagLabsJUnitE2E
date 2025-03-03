f9923c66529c305c2b22099218a5a812b56014cb
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2025-03-02 18:46:31 -0700

    Update SwagLabsTest.java

--- src/test/java/tests/SwagLabsTest.java
+++ src/test/java/tests/SwagLabsTest.java
@@ -40,11 +40,11 @@ public class SwagLabsTest extends BaseTest{
         // Randomly pick one valid username
         String[] users = {
                 "standard_user",
-                "locked_out_user",
-                "problem_user",
-                "performance_glitch_user",
-                "error_user",
-                "visual_user"
+//                "locked_out_user",
+//                "problem_user",
+//                "performance_glitch_user",
+//                "error_user",
+//                "visual_user"
         };
 
         int randomIndex = (int) (Math.random() * users.length);
